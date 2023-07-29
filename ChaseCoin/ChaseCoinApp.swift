3cb40fd0053e070998dc6e425c4e6bd36f30e5b3
Given Mahlangu <givendeluxe@gmail.com>
2023-07-29 23:26:50 +0200

    Initial Commit

---
+++ ChaseCoin/ChaseCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChaseCoinApp.swift
+//  ChaseCoin
+//
+//  Created by Given Mahlangu on 2023/07/29.
+//
+
+import SwiftUI
+
+@main
+struct ChaseCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChaseCoin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChaseCoin
+//
+//  Created by Given Mahlangu on 2023/07/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
